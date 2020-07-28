a4b76fdbad4dc4991dfdc05814f7cb96a200d149
Marina <marinafaenze@hotmail.com>
2020-07-27 22:11:37 -0300

    validate

--- src/pages/Form/Form.js
+++ src/pages/Form/Form.js
@@ -1,51 +1,128 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Form.css';
-import cardChip from '../../assets/images/card-chip.jpg';
+import cardChip from '../../assets/images/chip-card.png';
+import { useFormik } from 'formik';
+import validate from './Validate';
+import Flip from 'react-reveal/Flip';
 
 const Form = () => {
+
+    const [showCreditCard, setShowCreditCard] = useState(false);
+
+    const formik = useFormik({
+        initialValues: {
+            cardNumber: '**** **** **** ****',
+            fullName: '',
+            expiryDate: 'MM/YY',
+            cvc: '***',
+            dni: ''  
+        },
+        validate,
+        onSubmit: values => {
+          alert(JSON.stringify(values, null, 2));
+        },
+    });
+
 
     return (
         <div className="form-container">
 
             <div className="credit-card-content">
-
-                {/* <div className="credit-card-back">
-                    <div className="line-magnet"></div>
-                    <div className="data-cvc">
-                        <div className="cvc-number">***</div>
+            { (showCreditCard)
+            ?
+                (   <Flip left>
+                    <div className="credit-card-back">
+                        <div className="line-magnet"></div>
+                        <div className="data-cvc">
+                            <div className="cvc-number">{formik.values.cvc}</div>
+                        </div>
                     </div>
-                </div> */}
-
-                <div className="credit-card">
-                    <div className="chip-container"> 
-                        <img src={cardChip} alt="card chip" />
+                    </Flip>
+                )   
+            :
+                (   <div className="credit-card">
+                        <div className="chip-container"> 
+                            <img src={cardChip} alt="card chip" />
+                        </div>
+                        <div className="number-card">{formik.values.cardNumber}
+                        </div>
+                        <div className="card-data-container">
+                            <div className="fullName-card">{formik.values.fullName}</div>
+                            <div className="expiry-date-card">{formik.values.expiryDate}</div>
+                        </div>
                     </div>
-                    <div className="number-card">**** **** **** ****</div>
-                    <div className="card-data-container">
-                        <div className="fullName-card">Nombre y Apellido</div>
-                        <div className="expiry-date-card">MM/YY</div>
-                    </div>
-                </div>
+                )
+            }
             </div>
 
             <div className="form-content">
-                <form>
-                    <label>Número de la tarjeta</label>
-                    <input type="number"/>
+                
+                <form onSubmit={formik.handleSubmit}>
 
-                    <label>Nombre y Apellido</label>
-                    <input type="text"/>
+                    <label htmlFor="cardNumber">Número de la tarjeta</label>
+                    <input 
+                        type="number"
+                        name="cardNumber" 
+                        id="cardNumber"
+                        onChange={formik.handleChange}
+                        value={formik.values.cardNumber}
+                        onBlur={formik.handleBlur}
+                    />
+                    {   (formik.touched.cardNumber && formik.errors.cardNumber)
+                        ? <div className="formik-errors">{formik.errors.cardNumber}</div>
+                        : null
+                    }
 
-                    <label>Fecha de expiración</label>
-                    <input type="number"/>
+                    <label htmlFor="fullName">Nombre y Apellido</label>
+                    <input 
+                        type="text"
+                        name="fullName" 
+                        id="fullName"
+                        onChange={formik.handleChange}
+                        value={formik.values.fullName}
+                        onBlur={formik.handleBlur}
+                    />                    
+                    {   (formik.touched.fullName && formik.errors.fullName)
+                        ? <div className="formik-errors">{formik.errors.fullName}</div>
+                        : null
+                    }
 
-                    <label>Código de seguridad</label>
-                    <input type="number"/>
+                    <label htmlFor="expiryDate">Fecha de expiración</label>
+                    <input 
+                        type="number"
+                        name="expiryDate" 
+                        id="expiryDate"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
 
-                    <label>DNI del titular de la tarjeta</label>
-                    <input type="number"/>
+                    <label htmlFor="cvc">Código de seguridad</label>
+                    <input 
+                        type="number"
+                        name="cvc" 
+                        id="cvc"
+                        onChange={formik.handleChange}
+                        onBlur={() => setShowCreditCard(false)}
+                        onFocus={() => setShowCreditCard(true)}
+                        value={formik.values.cvc}
+                    />
+                    {   (formik.touched.cvc && formik.errors.cvc)
+                        ? <div className="formik-errors">{formik.errors.cvc}</div>
+                        : null
+                    }
 
-                    <button>Continuar</button>
+                    <label htmlFor="dni">DNI del titular de la tarjeta</label>
+                    <input 
+                        type="number"
+                        name="dni" 
+                        id="dni"
+                    />
+                    {   (formik.touched.dni && formik.errors.dni)
+                        ? <div className="formik-errors">{formik.errors.dni}</div>
+                        : null
+                    }
+
+                    <button type="submit">Continuar</button>
                 </form>
             </div>
 
---
+++ src/pages/Form/Validate.js
@@ -0,0 +1,38 @@
+const validate = (values) => {
+    const errors = {};
+
+    if(!values.cardNumber) {
+        errors.cardNumber = "Requerido";
+    } else if(typeof (values.cardNumber) !== "number" ) {
+        errors.cardNumber = "Tipo de dato incorrecto";
+    } else if (String(values.cardNumber).length !== 16 ) {
+        errors.cardNumber = "Error en el número de tarjeta";
+    }
+
+    if(!values.fullName) {
+        errors.fullName = "Requerido";
+    } else if (values.fullName.length < 4) {
+        errors.fullName = "Ingrese el nombre y apellido completo"
+    }
+
+    if(!values.cvc) {
+        errors.cvc = "Requerido";
+    } else if(typeof(values.cvc) !== "number" ) {
+        errors.cvc = "Tipo de dato incorrecto";
+    } else if (String(values.cvc).length !== 3) {
+        errors.cvc = "Debe ingresar los 3 dígitos de seguridad";
+    }
+
+    if(!values.dni) {
+        errors.dni = "Requerido";
+    } else if (typeof(values.dni) !== "number") {
+        errors.dni = "Tipo de dato inválido";
+    } else if (String(values.dni).length !== 7 || String(values.dni).length !== 8) {
+        errors.dni = "Documento inválido";
+    }
+
+    
+    return errors;
+}
+
+export default validate;
