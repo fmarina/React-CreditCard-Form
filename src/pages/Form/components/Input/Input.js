a30f09bf202df6e6654865a4514cd36a60ac99c6
Marina <marinafaenze@hotmail.com>
2020-07-28 18:30:29 -0300

    Input component created

--- src/pages/Form/components/Input/Input.js
+++ src/pages/Form/components/Input/Input.js
@@ -1,2 +1,32 @@
 import React from 'react';
 import './Input.css';
+
+const Input = ({type, name, id, handleChange, value, handleBlur, handleFocus}) => {
+    return (
+        <input
+            type={type} 
+            name={name}
+            id={id}
+            onChange={handleChange}
+            value={value}
+            onBlur={handleBlur}
+            onFocus={handleFocus}
+            autoComplete="off"
+        />
+    );
+}
+
+export default Input;
+
+                    {/* <input 
+                        type="number"
+                        name="cvc" 
+                        id="cvc"
+                        onChange={formik.handleChange}
+                        onBlur={(e) => (
+                            setShowCreditCard(false),
+                            formik.handleBlur(e)
+                        )}
+                        onFocus={() => setShowCreditCard(true)}
+                        value={formik.values.cvc}
+                    /> */}
