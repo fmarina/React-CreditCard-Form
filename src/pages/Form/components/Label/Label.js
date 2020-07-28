8e9b9c17ad4183d3a35eed955e60e09ea2720a08
Marina <marinafaenze@hotmail.com>
2020-07-28 18:02:01 -0300

    Label component created

--- src/pages/Form/components/Label/Label.js
+++ src/pages/Form/components/Label/Label.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Label.css';
+
+const Label = ({labelfor, text}) => {
+    return (
+        <label htmlFor={labelfor}>{text}</label>
+    );
+}
+
+export default Label;
